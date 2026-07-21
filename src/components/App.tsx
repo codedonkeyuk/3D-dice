14c347e527f8e45807257f55c8c9d3ee467523fc
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-07-21 17:28:12 +0100

    Added html dialog to alter dice settings, painful React is not grat with HTML Dialogs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
 import BabylonCanvas from "./BabylonCanvas";
+import SettingsDialog from "./SettingsDialog";
 
 const App = () => {
   return (
     <>
+      <SettingsDialog>
+        <h2>Settings</h2>
+      </SettingsDialog>
       <BabylonCanvas />
     </>
   );
---
+++ src/components/SettingsDialog.tsx
@@ -0,0 +1,41 @@
+import { useRef } from "react";
+
+interface SettingsDialogProps {
+  children?: React.ReactNode;
+}
+
+const SettingsDialog: React.FC<SettingsDialogProps> = ({ children }) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const openDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  };
+
+  const closeDialog = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+  };
+
+  return (
+    <div>
+      <button
+        className="dice-settings-button"
+        onClick={openDialog}
+        aria-label="Open Settings"
+      >
+        <span className="sr-only">Menu</span>
+      </button>
+      <dialog ref={dialogRef}>
+        {children}
+        <button data-command="close" onClick={(e) => closeDialog(e)}>
+          Close
+        </button>
+      </dialog>
+    </div>
+  );
+};
+
+export default SettingsDialog;
