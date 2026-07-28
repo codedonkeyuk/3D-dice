a7162e0f0d4d92c0f51ccbc21819dc262c689da9
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-07-28 23:43:34 +0100

    Should have fixed mac rending issue

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -34,15 +34,44 @@ export const ColorInputWrapperDiv = styled.div`
   border: 2px solid #2a2a30;
   cursor: pointer;
   display: flex;
+  position: relative;
+
+  &:focus-within {
+    border-color: #0056b3;
+    box-shadow: inset 0 0 0 2px #0056b3;
+  }
 `;
 
 export const ColorInput = styled.input`
-  width: 150%;
-  height: 150%;
-  cursor: pointer;
   border: none;
   background: none;
   padding: 0;
+  margin: 0;
+  cursor: pointer;
+
+  -webkit-appearance: none;
+  appearance: none;
+
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  width: calc(100% + 10px);
+  height: calc(100% + 10px);
+
+  &::-webkit-color-swatch-wrapper {
+    padding: 0;
+    margin: 0;
+  }
+
+  &::-webkit-color-swatch {
+    border: none;
+    border-radius: 0;
+    padding: 0;
+  }
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const DiceSelect = styled.select`
@@ -61,6 +90,10 @@ export const DiceSelect = styled.select`
   background-position: right 12px center;
   background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23a1a1aa' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
   background-size: 16px;
+  &:focus {
+    outline: 2px solid #0056b3;
+    outline-offset: 2px;
+  }
 `;
 
 export const DiceInput = styled.input`
